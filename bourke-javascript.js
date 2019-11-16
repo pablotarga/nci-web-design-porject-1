3aa910af3450feea715bb430b7e0a8212ec1d7ee
hawkwinglb <56236055+hawkwinglb@users.noreply.github.com>
2019-11-16 13:21:25 +0000

    Add files via upload
    This is what I've been working on.
    
    I got the dragon rider image from Deviantart (it's under a Creative Commons Noncommercial license). 
    
    I can't get my head around making the javascript work, but I suspect that's only to be expected after one single lecture on the topic...

---
+++ bourke-javascript.js
@@ -0,0 +1,33 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
